3f73066d9651cc91e5499c31bf5ab9ddbb2b1bb7
Kanhu@143 <kanhucharansahoo595@gmail.com>
2024-07-24 22:52:21 +0530

    do the initial configuration

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(
+  <>
+    <App />
+  </>
+);
