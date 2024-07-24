d48c85172465a4592be3cf5e3bb0370a38c0f7b7
Kanhu@143 <kanhucharansahoo595@gmail.com>
2024-07-24 22:56:46 +0530

    install the required dependency

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDom from "react-dom/client";
 import App from "./App";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
